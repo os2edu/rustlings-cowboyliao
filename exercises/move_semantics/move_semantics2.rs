0f3b63b0694e2e88ff94c7d6e7fb4f6019d611d5
cowboyliao <2730647052@qq.com>
2023-01-14 21:34:59 +0800

    23/1/14

--- exercises/move_semantics/move_semantics2.rs
+++ exercises/move_semantics/move_semantics2.rs
@@ -3,11 +3,30 @@
 // Execute `rustlings hint move_semantics2` or use the `hint` watch subcommand for a hint.
 
 // I AM NOT DONE
-
+/*
+So, `vec0` is passed into the `fill_vec` function as an argument. In Rust,
+when an argument is passed to a function and it's not explicitly returned,
+you can't use the original variable anymore. We call this "moving" a variable.
+Variables that are moved into a function (or block scope) and aren't explicitly
+returned get "dropped" at the end of that function. This is also what happens here
+.
+There's a few ways to fix this, try them all if you want:
+1. Make another, separate version of the data that's in `vec0` and pass that
+   to `fill_vec` instead.
+2. Make `fill_vec` borrow its argument instead of taking ownership of it,
+   and then copy the data within the function in order to return an owned
+   `Vec<i32>`
+3. Make `fill_vec` *mutably* borrow a reference to its argument (which will need t
+o be
+   mutable), modify it directly, then not return anything. Then you can get rid
+   of `vec1` entirely -- note that this will change what gets printed by the
+   first `println!`
+*/
 fn main() {
     let vec0 = Vec::new();
+    let vec2=vec0;
 
-    let mut vec1 = fill_vec(vec0);
+    let mut vec1 = fill_vec(vec2);
 
     // Do not change the following line!
     println!("{} has length {} content `{:?}`", "vec0", vec0.len(), vec0);
